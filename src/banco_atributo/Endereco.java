27dc505691622a5dc413acad3f17adbf7a38a58e
cristiane <cris_csr@yahoo.com.br>
2019-02-19 10:01:38 -0300

    primeiro commit

---
+++ src/banco_atributo/Endereco.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco_atributo;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class Endereco {
+    String rua;
+    int numero;
+    String bairro;
+    String cidade;
+    
+     public void setRua(String rua){
+       this.rua = rua; 
+    }
+}
