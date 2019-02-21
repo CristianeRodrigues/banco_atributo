135f19ee5b4a85d36ec53c22274adf72e663e9c1
cristiane <cris_csr@yahoo.com.br>
2019-02-21 11:23:16 -0300

    chamada do metodo setCodigo da classe funcionario

--- src/banco_atributo/Banco_atributo.java
+++ src/banco_atributo/Banco_atributo.java
@@ -16,6 +16,8 @@ public class Banco_atributo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCodigo(100);
     }
     
 }
